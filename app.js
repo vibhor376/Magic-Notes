4085c77ebaacb3694de6c204d7c3e680d18d09f6
vibhor376 <57498893+vibhor376@users.noreply.github.com>
2021-09-16 13:28:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,29 +4,25 @@ showNotes();
 let flag = false;
 let addBtn = document.getElementById('addBtn');
 addBtn.addEventListener('click', function (e) {
-    let addTxt = document.getElementById('addTxt');
-    let addTitle = document.getElementById('addTitle');
-    // console.log(addTxt.value);
-    let notes = localStorage.getItem("notes");
-    let title = localStorage.getItem("title");
+    let addTxt = document.getElementById('addTxt');//get hold of element having id as 'addTxt'
+    let addTitle = document.getElementById('addTitle');//get hold of element having id as 'addTitle'
+    let notes = localStorage.getItem("notes");//get 'notes' from local storage
     // console.log(notes);
-    if (notes == null) {
+    if (notes == null) {//intially null
         notesObj = [];
-        titleObj = [];
     } else {
-        notesObj = JSON.parse(notes);
-        titleObj = JSON.parse(title);
+        notesObj = JSON.parse(notes);//parse JSON string to object so that it can be stored in array
     }
-    // console.log(titleObj);
+    let myObj = {//make an object having two values
+        notes: addTxt.value,
+        title: addTitle.value
+    }
     if (addTitle.value != "" && addTxt.value != "") {
-        notesObj.push(addTxt.value);
-        titleObj.push(addTitle.value);
-        // console.log(addTitle.value);
-        localStorage.setItem('notes', JSON.stringify(notesObj));
-        localStorage.setItem('title', JSON.stringify(titleObj));
-        addTxt.value = "";
-        addTitle.value = "";
+        notesObj.push(myObj);//make an array of objects
         // console.log(notesObj);
+        localStorage.setItem('notes', JSON.stringify(notesObj));//converting notesObj into string
+        addTxt.value = "";//clear the notes section
+        addTitle.value = "";//clear the title section
         showNotes();
     } else {
         if (addTxt.value == "") {
@@ -39,77 +35,66 @@ addBtn.addEventListener('click', function (e) {
 //Function to show added notes
 function showNotes() {
     let notes = localStorage.getItem('notes');
-    let title = localStorage.getItem("title");
     if (notes == null) {
         notesObj = [];
-        titleObj = [];
     } else {
         notesObj = JSON.parse(notes);
-        titleObj = JSON.parse(title);
     }
-    let html = "";
-    // console.log(titleObj);
+    let html = "";// initialize a empty string
     for (let i = 0; i < notesObj.length; i++) {
         html += `
     <div class="card cardNote mx-2 my-2 " style="width: 18rem;">
         <div class="card-body">
-          <h5 class="card-title"> ${titleObj[i]}</h5>
-          <p class="card-text"> ${notesObj[i]} </p>
+          <h5 class="card-title">${i+1}. ${notesObj[i].title}</h5>
+          <p class="card-text"> ${notesObj[i].notes} </p>
           <div class="mb-0">
           <button id="${i}" onclick="deleteNote(this.id)" class="btn btn-primary" style="color:#ffff;" >Delete Note</button>
           <a id="${i}" onclick="markAsImportant(this.id)"  class="impBtn btn btn-danger" style="color:#ffff;" >Important</a>
           </div>
         </div>
-    </div>`;
+    </div>`;//concatenates all the items in array i.e to show the divs on screen 
     };
     let notesElem = document.getElementById('notes');
     if (notesObj.length != 0) {
-        notesElem.innerHTML = html;
+        notesElem.innerHTML = html;// to show the div elements on screen
     } else {
-        notesElem.innerHTML = `<p>Nothing to show! Use <b>Add a Note</b> section above to <b>add</b> notes.</p>
-        <p> Once note is <b>added</b>, click on <b>important</b> to mark note as important and <b>double</b> click on important to mark note as <b>not</b> important.</p>`;
+        notesElem.innerHTML = `<p>Nothing to show! Use Add a note section above to add notes.</p>
+        <p> Once note is added, click on important to mark note as important and then click again on important to mark note as not important.</p>`;
     }
 }
-// ondblclick="notImportant(this.id)"
 //Function to delete note
 
 function deleteNote(index) {
     // console.log('I am deleting this note', index);
     let notes = localStorage.getItem('notes');
-    let title = localStorage.getItem('title');
-    // let title= localStorage.getItem('title');
     if (notes == null) {
         notesObj = [];
-        titleObj = [];
     } else {
         notesObj = JSON.parse(notes);
-        titleObj = JSON.parse(title);
     }
-    notesObj.splice(index, 1);
-    titleObj.splice(index, 1);
-    localStorage.setItem('notes', JSON.stringify(notesObj));
-    localStorage.setItem('title', JSON.stringify(titleObj));
-    showNotes();
+    notesObj.splice(index, 1);// splice function used to delete elements from a particular index i.e in a range of (l,r)
+    localStorage.setItem('notes', JSON.stringify(notesObj));// update the local storage
+    showNotes();// call again 
 }
 
 // Function to search notes
 
 let searchText = document.getElementById('searchText');
 searchText.addEventListener('input', function (e) {
-    let val = searchText.value.toLowerCase();
+    let val = searchText.value.toLowerCase();// converts value of text to be searched into lower case
     //  console.log('fire',val);
-    let cardNote = document.getElementsByClassName('cardNote');
+    let cardNote = document.getElementsByClassName('cardNote');//get hold of element by class name
     // console.log(cardNote);
     Array.from(cardNote).forEach(function (element) {
-        let elementText = element.getElementsByTagName("p")[0].innerText;
-        let titleText = element.getElementsByTagName("h5")[0].innerText;
-        titleText = titleText.toLowerCase();
-        elementText = elementText.toLowerCase();
+        let elementText = element.getElementsByTagName("p")[0].innerText;// p tag in which you have to search
+        let titleText = element.getElementsByTagName("h5")[0].innerText;// h5 tag in which you have to search
+        titleText = titleText.toLowerCase();// lowercase conversion
+        elementText = elementText.toLowerCase();// lowercase conversion
         // console.log(elementText);
         if (elementText.includes(val) || titleText.includes(val)) {
-            element.style.display = "block";
+            element.style.display = "block";//display property changed to block
         } else {
-            element.style.display = "none";
+            element.style.display = "none";// used to hide other div elements on page when search condition does not match
         }
         // console.log(element);   
     })
@@ -120,20 +105,20 @@ searchText.addEventListener('input', function (e) {
 function markAsImportant(index) {
     let cardNote = document.getElementsByClassName('cardNote');
     // console.log(cardNote);
-    let count = 0;
+    let count = 0;// intialize count 
     let elm=document.getElementsByTagName('cardnote.a');
     // console.log(elm);
     Array.from(cardNote).forEach(function (element) {
-        if (count == index && !flag) {
+        if (count == index && !flag) {// if count matches the intialize bool value to true
             // console.log(element);
             element.style.backgroundColor = "#343a40";
             element.style.color = "#ffff";
-            flag=true;
+            flag=true;// true bool value indicates that div is marked important
         } else {
             if (count == index && flag) {
                 element.style.backgroundColor = "#ffff";
                 element.style.color = "#343a40";
-                flag=false;
+                flag=false;//revert the changes done above make bool value false so that important mark can be removed from element
             }
         }
         count++;
@@ -142,7 +127,7 @@ function markAsImportant(index) {
 }
 
 // Function to remove important mark
-
+// Just another implementation ignore it
 // function notImportant(index) {
 //     let cardNote = document.getElementsByClassName('cardNote');
 //     // console.log(cardNote);
